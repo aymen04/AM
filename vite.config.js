c8502f8c321535b69202a7835c1954c197f7b403
aymen04 <125768153+aymen050@users.noreply.github.com>
2025-10-15 18:01:42 -0400

    fixing routes

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,21 @@ export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+              return 'vendor';
+            }
+            if (id.includes('three') || id.includes('@react-three')) {
+              return 'three';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
